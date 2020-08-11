fe0bd2ab917f90427a8d8d1e5cd3139b286e58ba
jo-mine <minetab25@gmail.com>
2020-08-12 08:38:21 +0900

    test commit

---
+++ Test/Assets/Sample.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Sample : MonoBehaviour
+{
+    string s;
+    // Start is called before the first frame update
+    void Start()
+    {
+        s = "ya";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnGUI()
+    {
+        if (GUILayout.Button("button"))
+        {
+            s = DateTime.Now.ToString();
+        }
+        GUILayout.Label(s);
+    }
+}
